15664f1415ce30b2bb0d69a4e6b56971cba0820c
JA-Proger <vitalypolevoy@192.168.1.3>
2025-06-16 00:41:41 +0300

    changes

--- StaLang_Rust/target/debug/deps/StaLang_Rust-ea6fb084775d66d3.d
+++ StaLang_Rust/target/debug/deps/StaLang_Rust-ea6fb084775d66d3.d
@@ -1,5 +1,9 @@
-/Users/vitalypolevoy/Library/Mobile Documents/com~apple~CloudDocs/5_PROJECTS/7_PROJECTs_PROGs/StaLang/StaLang_Rust/target/debug/deps/libStaLang_Rust-ea6fb084775d66d3.rmeta: src/main.rs
+/Users/vitalypolevoy/Library/Mobile Documents/com~apple~CloudDocs/5_PROJECTS/7_PROJECTs_PROGs/StaLang/StaLang_Rust/target/debug/deps/libStaLang_Rust-ea6fb084775d66d3.rmeta: src/main.rs src/lexer/lexer.rs src/ast/ast.rs src/parser/parser.rs src/commands/comline.rs
 
-/Users/vitalypolevoy/Library/Mobile Documents/com~apple~CloudDocs/5_PROJECTS/7_PROJECTs_PROGs/StaLang/StaLang_Rust/target/debug/deps/StaLang_Rust-ea6fb084775d66d3.d: src/main.rs
+/Users/vitalypolevoy/Library/Mobile Documents/com~apple~CloudDocs/5_PROJECTS/7_PROJECTs_PROGs/StaLang/StaLang_Rust/target/debug/deps/StaLang_Rust-ea6fb084775d66d3.d: src/main.rs src/lexer/lexer.rs src/ast/ast.rs src/parser/parser.rs src/commands/comline.rs
 
 src/main.rs:
+src/lexer/lexer.rs:
+src/ast/ast.rs:
+src/parser/parser.rs:
+src/commands/comline.rs:
--- StaLang_Rust/target/debug/deps/StaLang_Rust-fa8af8b668894995.d
+++ StaLang_Rust/target/debug/deps/StaLang_Rust-fa8af8b668894995.d
@@ -1,5 +1,9 @@
-/Users/vitalypolevoy/Library/Mobile Documents/com~apple~CloudDocs/5_PROJECTS/7_PROJECTs_PROGs/StaLang/StaLang_Rust/target/debug/deps/libStaLang_Rust-fa8af8b668894995.rmeta: src/main.rs
+/Users/vitalypolevoy/Library/Mobile Documents/com~apple~CloudDocs/5_PROJECTS/7_PROJECTs_PROGs/StaLang/StaLang_Rust/target/debug/deps/libStaLang_Rust-fa8af8b668894995.rmeta: src/main.rs src/lexer/lexer.rs src/ast/ast.rs src/parser/parser.rs src/commands/comline.rs
 
-/Users/vitalypolevoy/Library/Mobile Documents/com~apple~CloudDocs/5_PROJECTS/7_PROJECTs_PROGs/StaLang/StaLang_Rust/target/debug/deps/StaLang_Rust-fa8af8b668894995.d: src/main.rs
+/Users/vitalypolevoy/Library/Mobile Documents/com~apple~CloudDocs/5_PROJECTS/7_PROJECTs_PROGs/StaLang/StaLang_Rust/target/debug/deps/StaLang_Rust-fa8af8b668894995.d: src/main.rs src/lexer/lexer.rs src/ast/ast.rs src/parser/parser.rs src/commands/comline.rs
 
 src/main.rs:
+src/lexer/lexer.rs:
+src/ast/ast.rs:
+src/parser/parser.rs:
+src/commands/comline.rs:
