c1b396ee08af940688eeb43995546783487bd32b
JA Proger <134533485+ja-proger@users.noreply.github.com>
2025-06-16 00:19:23 +0300

    Update main.rs

--- main.rs
+++ main.rs
@@ -2,6 +2,12 @@
 
 #[path = "lexer/lexer.rs"]
 mod lexer
+#[path = "ast/ast.rs"]
+mod ast
+#[path = "parser/parser.rs"]
+mod parser
+#[path = "commands/comline.ts"]
+mod comline
 
 fn main(){
   println!("compiler");
