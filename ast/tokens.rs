91a86755719e570b2caa46f9ed874c257cd65ea2
JA Proger <134533485+ja-proger@users.noreply.github.com>
2025-06-15 23:38:48 +0300

    Update tokens.rs

---
+++ ast/tokens.rs
@@ -0,0 +1,21 @@
+
+enum TokenKind {
+  KEYWORD,
+  OPERATOR,
+  SEPARATOR
+}
+
+struct TokenDocs {
+  token_id: i8,
+  kind: TokenKind,
+  name: String,
+  sign: String
+}
+
+struct TokenOperational {
+  sign: String,
+  pos_s: i8,
+  pos_f: i8,
+  line: i8
+}
+
