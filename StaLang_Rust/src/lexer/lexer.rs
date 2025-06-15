1be65250a04ebb1e75d9e5e247d6c86b75314f1b
JA-Proger <vitalypolevoy@192.168.1.3>
2025-06-16 00:59:22 +0300

    remove 'mut'

--- StaLang_Rust/src/lexer/lexer.rs
+++ StaLang_Rust/src/lexer/lexer.rs
@@ -2,7 +2,7 @@
 #[path = "../ast/tokens.rs"]
 mod tokens;
 fn lex(src: &str) -> Vec<tokens::TokenOperational> {
-    let mut tokens: Vec<tokens::TokenOperational> = vec![];
+    let tokens: Vec<tokens::TokenOperational> = vec![];
     for (index, char) in src.chars().enumerate(){
         println!("{index}: {char}");
     }
