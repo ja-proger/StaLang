fc9476eeb8daa92125221af9185f348bb63f4d9a
JA-Proger <vitalypolevoy@192.168.1.3>
2025-06-16 00:40:06 +0300

    changes

---
+++ StaLang_Rust/target/debug/deps/StaLang_Rust-ea6fb084775d66d3.d
@@ -0,0 +1,5 @@
+/Users/vitalypolevoy/Library/Mobile Documents/com~apple~CloudDocs/5_PROJECTS/7_PROJECTs_PROGs/StaLang/StaLang_Rust/target/debug/deps/libStaLang_Rust-ea6fb084775d66d3.rmeta: src/main.rs
+
+/Users/vitalypolevoy/Library/Mobile Documents/com~apple~CloudDocs/5_PROJECTS/7_PROJECTs_PROGs/StaLang/StaLang_Rust/target/debug/deps/StaLang_Rust-ea6fb084775d66d3.d: src/main.rs
+
+src/main.rs:
---
+++ StaLang_Rust/target/debug/deps/StaLang_Rust-fa8af8b668894995.d
@@ -0,0 +1,5 @@
+/Users/vitalypolevoy/Library/Mobile Documents/com~apple~CloudDocs/5_PROJECTS/7_PROJECTs_PROGs/StaLang/StaLang_Rust/target/debug/deps/libStaLang_Rust-fa8af8b668894995.rmeta: src/main.rs
+
+/Users/vitalypolevoy/Library/Mobile Documents/com~apple~CloudDocs/5_PROJECTS/7_PROJECTs_PROGs/StaLang/StaLang_Rust/target/debug/deps/StaLang_Rust-fa8af8b668894995.d: src/main.rs
+
+src/main.rs:
