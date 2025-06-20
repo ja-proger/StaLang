f3825365a477a36913baddde179d526dc0f9b41b
JA-Proger <-->
2025-06-20 09:00:40 +0300

    Added colorful commands and about command

--- commandline/commandline.swift
+++ commandline/commandline.swift
@@ -11,12 +11,12 @@ public struct Command {
 
 public func runCommandHelp(){
     for com in COMMANDS {
-        print("(\(com.commandId)). \(com.name)\t'\(com.sign)'\t\(com.docs)\n")
+        print("(\(com.commandId)). \u{001B}[1;32m\(com.name)\u{001B}[0m\t\u{001B}[1;92m'\(com.sign)'\u{001B}[0m\n\t\(com.docs)")
     }
 }
 
-public func runCommandDev(){
-    
+public func runCommandAbout(){
+    print("StaLang or Stal is a DSL programming language which supports meta, functional and object-orientated programming paradigms. It can be used for data anlysis, creating forecasts especially in economics, math, programming, and even for AI.\nIt was created in 2025 by person, who hides his identity by JA Proger nickname. You can find the project here \u{001B}[4;96mhttps://github.com/ja-proger/StaLang.git\u{001B}[0m")
 }
 
 
@@ -25,5 +25,6 @@ public let COMMANDS: [Command] = [
     Command(commandId: 1, argsQ: 1, name: "Explain", sign: "-explain [id]", docs: "Exmplain error or show documentation"),
     Command(commandId: 2, argsQ: 0, name: "Repl", sign: "-repl", docs: "Command line mode of the stal. Iterpretation mode"),
     Command(commandId: 3, argsQ: 0, name: "Dev", sign: "-dev", docs: "Development mode"),
-    Command(commandId: 4, argsQ: 1, name: "Compile", sign: "-compile", docs: "Compile the source code")
+    Command(commandId: 4, argsQ: 1, name: "Compile", sign: "-compile", docs: "Compile the source code"),
+    Command(commandId: 5, argsQ: 0, name: "About StaLang", sign: "-about", docs: "Run to know history of the language")
 ]
--- main.swift
+++ main.swift
@@ -11,17 +11,25 @@ func main() {
     // var tokens = run(src: src)
     let args = CommandLine.arguments
     print("\nStaLang Compiler\n----------------------")
+    print("(1) EXECUTED COMMAND:\t'stal \(args[1])'")
+    print("(2) FILE: \(args[0] == "stal" ? "Command line" : args[0])")
+    print("----------------------")
     if args.count < 2 {
         print("ERROR [CL-0]: No commands entered")
     } else {
         if args[1] == "-help"{
             runCommandHelp()
-        } else if args[1] == "-dev"{
+        }
+        else if args[1] == "-dev"{
             let file = (try? String(contentsOfFile: "./example.stal", encoding: .utf8))!
-            print("\nDEV COMPILATION\n------------\n")
-            print("\(file)\n----------------")
+            print("\nDEV COMPILATION\n-----------------------\n")
+            print("\(file)\n----------------------")
             print(run(src: file))
-        } else {
+        }
+        else if args[1] == "-about"{
+            runCommandAbout()
+        }
+        else {
             print("ERROR [CL-1]: Unknown command")
         }
     }
